f99a0ab8a93dedba017691b3c71f8575b6f6bbeb
Thomas Dunlap <ttdunlap33@gmail.com>
2021-03-19 17:58:33 -0600

    save button

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,8 @@
-$('.saveBtn').on
+$('.saveBtn').on('click' , function (event) {
+    var value = $(event.target).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+
+    localStorage.setItem(time, value);
+    console.log(time); 
+    console.log(value);
+});
