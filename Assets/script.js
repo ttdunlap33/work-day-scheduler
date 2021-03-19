27a2ec05e0bafe0a145195d0dfde51ac78920372
Thomas Dunlap <ttdunlap33@gmail.com>
2021-03-18 21:21:54 -0600

    pushing some js and jquery changes

---
+++ Assets/script.js
@@ -0,0 +1,17 @@
+const colorcode = document.getElementsByClassName("colorcode");
+
+
+var currentHour = (new Date()).getHours();
+$('.colorcode')
+  .each(function(){
+    var val = parseInt($(this).prop('id'));
+    if(val > currentHour && val < currentHour + 6){
+      $(this).css('background-color','Blue');
+    }else if(val < currentHour && val > currentHour-6){
+      $(this).css('background-color','Red');
+    }else if(val === currentHour){
+      $(this).css('background-color','Green');
+    }else{
+      $(this).css('background-color','White');
+    }
+  });
